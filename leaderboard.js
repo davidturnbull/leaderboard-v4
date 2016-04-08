1baca2b0f6bdfbb696045216e72967ebafed2780
dturnbull <hello@davidturnbull.com>
2016-04-08 21:12:48 +1000

    8 - Databases, Part 2

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,7 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
     Template.leaderboard.helpers({
         'player': function(){
-            return PlayersList.find();
+            return PlayersList.find({}, { sort: {score: -1, name: 1} });
         },
         'selectedClass': function(){
             var playerId = this._id;
@@ -11,12 +11,24 @@ if(Meteor.isClient){
             if(playerId == selectedPlayer){
                 return "selected"
             }
-        }  
+        },
+        'selectedPlayer': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            return PlayersList.findOne({ _id: selectedPlayer });
+        }
     });
     Template.leaderboard.events({
         'click .player': function(){
             var playerId = this._id;
             Session.set('selectedPlayer', playerId);
+        },
+        'click .increment': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5} });
+        },
+        'click .decrement': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
         }
     });
 }
