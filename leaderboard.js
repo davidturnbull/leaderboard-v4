4eacefac28087d95bdcaea6369a004a5e6537f56
dturnbull <hello@davidturnbull.com>
2016-04-08 21:40:21 +1000

    10 - Accounts

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,9 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
     Template.leaderboard.helpers({
         'player': function(){
-            return PlayersList.find({}, { sort: {score: -1, name: 1} });
+            var currentUserId = Meteor.userId();
+            return PlayersList.find({ createdBy: currentUserId },
+                                    { sort: {score: -1, name: 1} });
         },
         'selectedClass': function(){
             var playerId = this._id;
@@ -39,9 +41,11 @@ if(Meteor.isClient){
         'submit form': function(){
             event.preventDefault();
             var playerNameVar = event.target.playerName.value;
+            var currentUserId = Meteor.userId();
             PlayersList.insert({
                 name: playerNameVar,
-                score: 0
+                score: 0,
+                createdBy: currentUserId
             });
             event.target.playerName.value = "";
         }
